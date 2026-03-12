29776b0fa8fb2e9f65be1dcf5c78fb96c62d9e1e
lollipop690 <kpkj.crazy@gmail.com>
2026-03-13 01:25:11 +0800

    some minor changes

--- redis_setup.py
+++ redis_setup.py
@@ -2,3 +2,8 @@ import redis
 import yfinance as yf
 import threading
 
+r = redis.Redis(host='localhost',port=6379,db=0,decode_responses=True) #decode_responses=True is to return decoded responses
+
+def get_prices_for_tickers(tickers: list):
+    return {t: r.get("price:{}".format(t)) for t in tickers}
+
