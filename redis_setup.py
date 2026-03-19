aafc43fe30ec23e24d1674165ffb177323b087d8
lollipop690 <kpkj.crazy@gmail.com>
2026-03-20 03:01:31 +0800

    some updates

--- redis_setup.py
+++ redis_setup.py
@@ -7,3 +7,5 @@ r = redis.Redis(host='localhost',port=6379,db=0,decode_responses=True) #decode_r
 def get_prices_for_tickers(tickers: list):
     return {t: r.get("price:{}".format(t)) for t in tickers}
 
+def start_stream(tickers):
+    ...
