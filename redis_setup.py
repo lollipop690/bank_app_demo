219593789e23854613ebb8cdc7d3387531ced7ff
lollipop690 <kpkj.crazy@gmail.com>
2026-03-20 03:06:16 +0800

    small update

--- redis_setup.py
+++ redis_setup.py
@@ -4,8 +4,15 @@ import threading
 
 r = redis.Redis(host='localhost',port=6379,db=0,decode_responses=True) #decode_responses=True is to return decoded responses
 
+_ws_thread = None
+_subscribed = set()
+_lock = threading.Lock()
+
 def get_prices_for_tickers(tickers: list):
     return {t: r.get("price:{}".format(t)) for t in tickers}
 
-def start_stream(tickers):
-    ...
+def _on_msg(msg):
+    ticker = msg.get('id')
+    price = msg.get('price')
+    if ticker and price:
+        r.set("price: {}".format(ticker),price)
